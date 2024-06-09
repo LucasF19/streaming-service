643e21384cac070994ccb01803050fd20e4dbec8
Lucas Ferreira <lucas.maia@venhapranuvem.com.br>
2024-06-09 16:30:21 -0300

    STR-2513 feat: finalizando filtro

--- src/app/modules/home/pages/home/home.ts
+++ src/app/modules/home/pages/home/home.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
 import { Genre } from '../../interfaces/home-interface';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+import { Router } from '@angular/router';
+
+export interface Movie {
+  title: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,20 +16,52 @@ import { Genre } from '../../interfaces/home-interface';
   styleUrls: ['./home.scss'],
 })
 export class AppHome implements OnInit {
-  releasesMovies = [];
+  searchControl: FormControl = new FormControl('');
+  releasesMovies: any[] = [];
   genresMovies: { [key: string]: any[] } = {};
-  avatar = '/docs/images/people/profile-picture-5.jpg';
+  filteredMovies: any[] = [];
+  allMovies!: Array<any>;
+  emptyFilter!: boolean;
   isLoading = true;
+  searching!: boolean;
 
   typesMovie: Genre[] = [];
 
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService, private router: Router) { }
 
   ngOnInit(): void {
     this.getTypesList();
     this.getAllMovies();
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300)
+    ).subscribe(value => {
+      this.filterMovies(value);
+    });
   }
 
+
+  filterMovies(query: string) {
+    this.searching = true;
+    this.emptyFilter = false;
+
+    query = query.toLowerCase();
+    this.filteredMovies = this.allMovies.filter(movie =>
+      movie.title.toLowerCase().includes(query)
+    );
+
+    if(this.filteredMovies.length === 0){
+      this.emptyFilter = true
+    }
+
+    if(query.length === 0){
+      this.searching = false;
+    }
+  }
+
+  movieSelectSearch(movieId: string){
+    this.router.navigate(['/card-description', movieId]);
+  }
 
   getTypesList() {
     this.movieService.getTypesList().subscribe((data: any) => {
@@ -31,9 +71,20 @@ export class AppHome implements OnInit {
   }
 
   getMoviesByGenres() {
+    const allMoviesSet: Set<any> = new Set();
+    this.allMovies = [];
+  
     this.typesMovie.forEach(genre => {
       this.movieService.getMoviesByGenre(genre.id).subscribe((data: any) => {
         this.genresMovies[genre.name] = data.results;
+
+        data.results.forEach((movie: Movie) => {
+          if (!allMoviesSet.has(movie.id)) {
+            allMoviesSet.add(movie.id);
+            this.allMovies.push(movie);
+          }
+        });
+
         this.isLoading = false;
       });
     });
