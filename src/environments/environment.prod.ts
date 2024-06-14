098c974f9be175f526ac07e3e8406b84482a6ff3
Lucas Ferreira <lucas.maia@venhapranuvem.com.br>
2024-06-14 20:26:29 -0300

    Adicionando variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyABWO41E7TWio8eT1ccVOIq1VbFFzi5w_E",
+    authDomain: "streaming-service-40d6e.firebaseapp.com",
+    projectId: "streaming-service-40d6e",
+    storageBucket: "streaming-service-40d6e.appspot.com",
+    messagingSenderId: "902183636140",
+    appId: "1:902183636140:web:d7efcd6c91c002f9042309"
+  }
 };
